441d3d5e2f912a4b822be03491d9aaa381b07ce9
Celia Wilford <celiawilford@gmail.com>
2023-02-24 15:43:50 +0100

    Bundler fix

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -10,6 +10,12 @@ class BookmarksController < ApplicationController
     end
   end
 
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
   private
 
   def params_bookmark
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
+
+  resources :bookmarks, only: [:destroy]
 end
