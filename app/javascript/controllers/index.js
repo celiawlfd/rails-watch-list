8b151cb53217d5afec8fbd9dcb0ac9cb4f6b02a2
Celia Wilford <celiawilford@gmail.com>
2023-02-25 17:31:13 +0100

    failed js try

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import BackController from "./back_controller"
+application.register("back", BackController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
