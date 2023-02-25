7cc7fb538353a7abf7ac9306d8982c1531330953
Celia Wilford <celiawilford@gmail.com>
2023-02-25 17:36:59 +0100

    border added to image listcard & consol.log removed

--- app/javascript/controllers/listcard_controller.js
+++ app/javascript/controllers/listcard_controller.js
@@ -4,16 +4,13 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["title"]
   connect() {
-    console.log("hello");
   }
 
   arrow() {
     this.titleTarget.insertAdjacentHTML("beforeend", "<i class='ps-2 fa-solid fa-arrow-right-long'></i>");
-    console.log("arrow");
   }
 
   noarrow() {
     this.titleTarget.innerHTML = ""
-    console.log("no arrow");
   }
 }
