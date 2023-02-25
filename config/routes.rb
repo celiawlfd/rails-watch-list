9c95cf2828d9c6fa2f4fef96394c1ed91cb221ad
Celia Wilford <celiawilford@gmail.com>
2023-02-25 12:56:24 +0100

    black & rounded border added for show + back ground image / index: lay-out & style des cards

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:new, :create]
+    resources :bookmarks, only: [:create]
   end
 
   resources :bookmarks, only: [:destroy]
